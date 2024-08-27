58ad9ff676264d57e3cde3d482bcd61a87a939a8
Sergey <emelyanov.sm2002@gmail.com>
2024-08-27 17:28:05 +0300

    Добавил представления для сущности категории-поста с русской локализацией

---
+++ admin/views/post-category/update.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\helpers\Html;
+
+/** @var yii\web\View $this */
+/** @var admin\models\PostCategory $model */
+
+$this->title = Yii::t('app', 'Update Post Category: {name}', ['name' => $model->name]);
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Post Categories'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => Html::encode($model->name), 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = Yii::t('app', 'Update');
+?>
+<div class="post-category-update">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
